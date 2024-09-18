01e788bde236c2d08d94d0b2e1bea01e3a374373
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-09-18 10:40:58 -0300

    inicio com instalações e criação de porta

---
+++ back-end/server.js
@@ -0,0 +1,35 @@
+import express from "express"; // 
+import cors from "cors";
+
+const app = express();  
+const PORT = 3000
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (_, res)=>{
+    return res.send('Hello Brazilian...')
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Rodando na port ${PORT}`)
+})
+
+/**
+ * Inicio básico do projeto
+ * 
+ * * npm init -y 
+ * * npm install express --save
+ * * npm i swagger-autogen swagger-ui-express mongoose mongoose-to-swagger cors dotenv
+ * * install nodemon --save-dev
+ * 
+ * *****  no packege.js
+ * 
+ *  *  "type":"module",  colocado para poder usar import ao inves de require
+ * 
+ *  para rodar o projeto npm run dev
+ * 
+ */
