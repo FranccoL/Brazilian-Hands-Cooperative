2adebeba0f2b1a038f6281ee049e408902188b32
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-09-19 14:30:22 -0300

    ajustes de sintaxe e validationError

--- back-end/src/validatorError/validationError.js
+++ back-end/src/validatorError/validationError.js
@@ -1,15 +1,15 @@
 export const validationError = (res, err) => {
     const errorStr = String(err);
 
-    // Entrar quando o Mongoose der algum erro caso eu coloque validation do mongoose
-    if (errorStr.includes(`ValidationError:`)) {
-      const cleanedMessage = errorStr.replace("ValidationError: ", "").replace(/:/g, '');
-      return res.status(400).json({
-        status: "Erro",
-        statusMensagem: cleanedMessage,
-        resposta: errorStr
-      });
-    }
+    // // Entrar quando o Mongoose der algum erro caso eu coloque validation do mongoose
+    // if (errorStr.includes(`ValidationError:`)) {
+    //   const cleanedMessage = errorStr.replace("ValidationError: ", "").replace(/:/g, '');
+    //   return res.status(400).json({
+    //     status: "Erro",
+    //     statusMensagem: cleanedMessage,
+    //     resposta: errorStr
+    //   });
+    // }
   
     // Erro definido manualmente por você
     if (errorStr.includes(`Error:`)) {
